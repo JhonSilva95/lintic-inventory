011a0f5b42646977d08cc5f8596b69381d36062a
JhonSilva95 <jhon.sicam95@gmail.com>
2026-01-20 13:42:41 -0500

    Initial commit

---
+++ src/main/java/linktic/inventory/LinkticInventoryApplication.java
@@ -0,0 +1,13 @@
+package linktic.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkticInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkticInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/linktic/inventory/LinkticInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package linktic.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkticInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
